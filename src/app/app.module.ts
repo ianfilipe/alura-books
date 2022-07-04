d55f463f88d24f324079d949d69dac95e9373888
Ian <ssianfilipe@gmail.com>
2022-07-04 12:47:59 -0300

    create carousel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
